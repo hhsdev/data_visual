9946d6637e98ee8fa0dd84b6d68c7a1996692a75
hhsdev <heinhtetsoe.dev@gmail.com>
2019-10-26 15:30:30 +0630

    Add `__main__.py` and update dependencies

--- __main__.py
+++ __main__.py
@@ -1,46 +1,117 @@
 # _*_ coding: utf-8 _*_
 
-from data_visual.simulator import Simulator
-from data_visual.physical_node import PhysicalNode
-from data_visual.rubberband import Rubberband
+import click
+import networkx as nx
+
 from data_visual.attractor import Attractor
 from data_visual.graph import Graph
+from data_visual.graph_artist import GraphArtist
+from data_visual.setting import Setting
+from data_visual.physical_node import PhysicalNode
+from data_visual.rubberband import Rubberband
+from data_visual.simulator import Simulator
 
-NUM_NODES = 6
-LABELS = [i for i in range(0, NUM_NODES)]
-EDGES = [[0, 5], [0, 4], [0, 1], [1, 4], [1, 2], [2, 3], [3, 4]]
+from random import randrange
 
-l = SimpleGraph(
-        [PhysicalNode(label) for label in LABELS],
-        CONNECTIONS,
-        False)
+config = Setting(
+    {
+        "gravity": {"intensity": 2},
+        "node": {"intensity": 50000},
+        "edge": {"intensity": 0.04},
+    })
 
-def addForceReceiversTo(simulator):
-    for node in l.nodes:
-        simulator.registerReceiver(node)
+#bg = nx.complete_graph(15)
+#bg = nx.balanced_tree(2, 4)
+#bg = nx.cycle_graph(25)
+#bg = nx.dorogovtsev_goltsev_mendes_graph(5)
+def rand_complex():
+    return complex(randrange(0, 1000), randrange(0, 1000))
 
-def addForceEmittersTo(simulator):
-    addRepellers(simulator)
-    addRubberBands(simulator)
-    addCenterGravity(simulator)
+def translate(x, y):
+    return x * 5 + y
 
-def addRepellers(simulator):
-    for node in l.nodes:
-        simulator.registerEmitter(node)
+def get_grid_graph(n, m):
+    nx_graph = nx.grid_2d_graph(n, m)
+    nodes = [PhysicalNode(translate(x,y), rand_complex(), config["node"]["intensity"])
+                for (x,y) in nx_graph.nodes]
+    edges = [[translate(a, b), translate(c, d)]
+                for ((a, b), (c, d)) in nx_graph.edges]
+    return (nodes, edges)
 
-def addRubberBands(simulator)
+NODES, EDGES = get_grid_graph(5, 5)
+
+#bg = nx.petersen_graph()
+#bg = nx.sedgewick_maze_graph()
+#bg = nx.tetrahedral_graph()
+#NODES = [PhysicalNode(label, randVec2(), config["node"]["intensity"]) for label in bg.nodes]
+#EDGES = bg.edges
+#NODES = [PhysicalNode(0, randVec2(), 1), PhysicalNode(1, randVec2(), 1)]
+#EDGES = [(0,1)]
+print("  Simulating %s nodes and %s edges..." % (len(NODES), len(EDGES)))
+graph = Graph(NODES, EDGES, False)
+
+def add_force_receivers_to(simulator):
+    for node in graph.nodes:
+        simulator.register_receiver(node)
+
+def add_center_gravity(simulator):
+    center_gravity = Attractor(config["gravity"]["intensity"])
+    simulator.register_emitter(center_gravity)
+
+def add_force_emitters_to(simulator):
+    add_repellers(simulator)
+    add_rubber_bands(simulator)
+    add_center_gravity(simulator)
+
+def add_repellers(simulator):
+    for node in graph.nodes:
+        simulator.register_emitter(node)
+
+def add_rubber_bands(simulator):
     for edge in EDGES:
-        nodeA = l.nodes[edge[0]]
-        nodeB = l.nodes[edge[1]]
-        simulator.registerEmitter(Rubberband(nodeA, nodeB))
+        nodeA = graph.nodes[edge[0]]
+        nodeB = graph.nodes[edge[1]]
+        simulator.register_emitter(Rubberband(nodeA, nodeB, config["edge"]["intensity"]))
 
-def addAttracter(simulator):
-    centerGravity = Attractor()
-    simulator.registerEmitter(centerGravity)
-    
-if __name__ == "__main__":
+def simulate_with_progress_bar(simulator):
+    with click.progressbar(length=simulator.settings["max_ticks"]) as bar:
+        update_progress = lambda simulator : bar.update(simulator.ticks)
+        simulator.event_listeners["tick"].append(update_progress)
+        simulator.simulate()
+
+
+if  __name__ == "__main__":
+    artist_settings = Setting({
+        "background": "white",
+        "width": 1000,
+        "height": 1000,
+        "node": {"color": (0x46, 0x63, 0x65)},
+        "edge": {"color": (0x9d, 0x99, 0xb6)},
+    })
+
+    artist = GraphArtist(artist_settings)
+
     simulator = Simulator()
-    addForceReceiversTo(simulator)
-    addForceEmittersTo(simulator)
+    add_force_receivers_to(simulator)
+    add_force_emitters_to(simulator)
 
-    simulator.simulate()
+    frames = []
+    draw_frame = lambda stud: frames.append(artist.draw(graph))
+    simulator.event_listeners["tick"].append(draw_frame)
+
+    simulate_with_progress_bar(simulator)
+    image = artist.draw(graph)
+
+    print("  Writing gif... ", end="", flush=True)
+    frames[0].save(
+            './images/move.gif',
+            format='GIF',
+            append_images=frames[1:],
+            save_all=True,
+            duration=100,
+            loop=0)
+
+    print("saved to ./images/move.gif", flush=True)
+    print("  Writing final image... ", end="", flush=True)
+    image.save("./images/result.png")
+    print("saved to ./images/result.png", flush=True)
